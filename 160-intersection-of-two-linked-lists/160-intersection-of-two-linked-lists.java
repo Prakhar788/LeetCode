fce0b607bd321562a2768053b025fa3667f517e6
Prakhar Sharma <102613412+Prakhar788@users.noreply.github.com>
2022-05-28 22:36:30 +0530

    Update 160-intersection-of-two-linked-lists.java

--- 160-intersection-of-two-linked-lists/160-intersection-of-two-linked-lists.java
+++ 160-intersection-of-two-linked-lists/160-intersection-of-two-linked-lists.java
@@ -56,3 +56,38 @@ public class Solution {
 	
     }
 }
+
+
+
+
+
+// public class Solution {
+    
+//         public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
+        
+//         ListNode dummy1 = headA;
+//         ListNode dummy2 = headB;
+        
+       
+//         while(dummy1 != dummy2){
+            
+            
+//             if(dummy1 != null){
+                
+//                 dummy1 = dummy1.next;
+//             }else{
+               
+//                 dummy1 = headB;
+//             }
+//             if(dummy2 != null){
+               
+//                 dummy2 = dummy2.next;
+//             }else{
+               
+//                 dummy2 = headA;
+//             }
+//         }
+       
+//         return dummy2;
+//     }
+//     }
