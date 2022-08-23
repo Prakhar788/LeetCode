2229588d5b92222b44e002a2bfcc9860d202826f
Prakhar Sharma <102613412+Prakhar788@users.noreply.github.com>
2022-08-23 09:39:04 +0530

    Time: 0 ms (100.00%), Space: 40.1 MB (89.94%) - LeetHub

---
+++ 1491-average-salary-excluding-the-minimum-and-maximum-salary/1491-average-salary-excluding-the-minimum-and-maximum-salary.java
@@ -0,0 +1,17 @@
+class Solution {
+    public double average(int[] salary) {
+        int minS=Integer.MAX_VALUE;
+        int maxS=Integer.MIN_VALUE;
+        for(int i=0;i<salary.length;i++){
+            minS=Math.min(minS,salary[i]);
+            maxS=Math.max(maxS,salary[i]);
+        }
+        double sum=0;
+        for(int i=0;i<salary.length;i++){
+            if(minS!=salary[i] && maxS!=salary[i]){
+                sum+=salary[i];
+            }
+        }
+        return sum/(salary.length-2);
+    }
+}
