a082be9dd6630bc35c866ca35b31d8d36548d01e
Prakhar Sharma <102613412+Prakhar788@users.noreply.github.com>
2022-08-23 09:23:47 +0530

    Time: 2 ms (60.04%), Space: 43.1 MB (6.50%) - LeetHub

---
+++ 771-jewels-and-stones/771-jewels-and-stones.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int numJewelsInStones(String jewels, String stones) {
+     int res=0;
+        for(char j:jewels.toCharArray()){
+            for(char s:stones.toCharArray()){
+                if(s==j){
+                    res++;
+                }
+            }
+        }
+        return res;
+    }
+    
+}
