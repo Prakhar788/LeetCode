d43690341fe8282b96460daba1872348198a279d
Prakhar Sharma <102613412+Prakhar788@users.noreply.github.com>
2022-09-05 09:36:59 +0530

    Time: 13 ms (21.60%), Space: 40.5 MB (71.70%) - LeetHub

---
+++ 509-fibonacci-number/509-fibonacci-number.java
@@ -0,0 +1,12 @@
+//Top-Down Naive Approach
+class Solution {
+    public int fib(int n) {
+     if(n<=1){
+         return n;
+     }   
+        if(n==2){
+            return 1;
+        }
+        return fib(n-1)+fib(n-2);
+    }
+}
