6931a128193b00b37e1fdc40f9b234d9101157fa
Prakhar Sharma <102613412+Prakhar788@users.noreply.github.com>
2022-09-13 00:54:30 +0530

    Time: 4 ms (57.26%), Space: 56.4 MB (40.06%) - LeetHub

--- 169-majority-element/169-majority-element.java
+++ 169-majority-element/169-majority-element.java
@@ -1,6 +1,16 @@
 class Solution {
     public int majorityElement(int[] nums) {
-        Arrays.sort(nums);
-        return nums[nums.length/2];
+        int c=0;
+        int ele=0;
+       for(int i=0;i<nums.length;i++){
+        if(c==0){
+            ele=nums[i];
+        }
+        if(ele==nums[i]){
+            c++;
+        }
+        else c--;
     }
+        return ele;
+ }    
 }
