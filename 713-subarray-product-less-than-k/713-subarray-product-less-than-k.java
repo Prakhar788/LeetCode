daa0ab00383e390221db10d9d4e51c45f461d9c8
Prakhar Sharma <102613412+Prakhar788@users.noreply.github.com>
2022-09-15 00:44:27 +0530

    Time: 6 ms (98.90%), Space: 49 MB (91.27%) - LeetHub

--- 713-subarray-product-less-than-k/713-subarray-product-less-than-k.java
+++ 713-subarray-product-less-than-k/713-subarray-product-less-than-k.java
@@ -1,26 +1,27 @@
-// class Solution {
-//     public int numSubarrayProductLessThanK(int[] nums, int k) {
-//       int ans=0,i=0,j=0,p=1;
-//         while(j<nums.length){
-//             p*=nums[j];
-//             while(p>=k){
-//                 p/=nums[i++];  
-//             }
-//             ans+=j-i+1;
-//             j++;
-//         }
-//         return ans;
-//     }
-// }
 class Solution {
     public int numSubarrayProductLessThanK(int[] nums, int k) {
-        if (k <= 1) return 0;
-        int prod = 1, ans = 0, left = 0;
-        for (int right = 0; right < nums.length; right++) {
-            prod *= nums[right];
-            while (prod >= k) prod /= nums[left++];
-            ans += right - left + 1;
+      if (k <= 1) return 0;
+          int ans=0,i=0,j=0,p=1;
+        while(j<nums.length){
+            p*=nums[j];
+            while(p>=k){
+                p/=nums[i++];  
+            }
+            ans+=j-i+1;
+            j++;
         }
         return ans;
     }
 }
+// class Solution {
+//     public int numSubarrayProductLessThanK(int[] nums, int k) {
+//         if (k <= 1) return 0;
+//         int prod = 1, ans = 0, left = 0;
+//         for (int right = 0; right < nums.length; right++) {
+//             prod *= nums[right];
+//             while (prod >= k) prod /= nums[left++];
+//             ans += right - left + 1;
+//         }
+//         return ans;
+//     }
+// }
