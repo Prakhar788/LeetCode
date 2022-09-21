1932a3b4f25e3568607740a8f8ba3e8d56f699c5
Prakhar Sharma <102613412+Prakhar788@users.noreply.github.com>
2022-09-21 11:28:40 +0530

    Time: 10 ms (46.38%), Space: 45.2 MB (24.62%) - LeetHub

---
+++ 72-edit-distance/72-edit-distance.java
@@ -0,0 +1,28 @@
+class Solution {
+    public int min(int a,int b,int c){
+        int d=Math.min(a,b);
+        return Math.min(d,c);
+    }
+    public int minDistance(String word1, String word2) {
+    char[]str1=word1.toCharArray(); 
+    char[]str2=word2.toCharArray(); 
+    int temp[][]=new int[str1.length+1][str2.length+1];
+        for(int i=0;i<temp[0].length;i++){
+            temp[0][i]=i;
+        }
+         for(int i=0;i<temp.length;i++){
+            temp[i][0]=i;
+        }
+        for(int i=1;i<=str1.length;i++){
+            for(int j=1;j<=str2.length;j++){
+                if(str1[i-1]==str2[j-1]){
+                    temp[i][j]=temp[i-1][j-1];
+                }
+                else{
+                    temp[i][j]=1+min(temp[i-1][j-1],temp[i-1][j],temp[i][j-1]);
+                }
+            }
+        }
+        return temp[str1.length][str2.length];
+    }
+}
