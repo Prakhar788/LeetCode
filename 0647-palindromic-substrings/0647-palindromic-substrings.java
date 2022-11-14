056ececbd31c2474ae5797bafa920b6ce8e5a573
Prakhar Sharma <102613412+Prakhar788@users.noreply.github.com>
2022-11-14 22:35:01 +0530

    Time: 10 ms (45.95%), Space: 42.3 MB (45.42%) - LeetHub

---
+++ 0647-palindromic-substrings/0647-palindromic-substrings.java
@@ -0,0 +1,29 @@
+class Solution {
+    public static int countSubstrings(String s) {
+		int count = 0;
+		// odd
+		for (int axis = 0; axis < s.length(); axis++) {
+			for (int orbit = 0; orbit < s.length() && axis - orbit >= 0 && axis + orbit < s.length(); orbit++) {
+				if (s.charAt(axis - orbit) != s.charAt(axis + orbit)) {
+					break;
+				}
+				count++;
+
+			}
+
+		}
+		// even
+		for (double axis = 0.5; axis < s.length(); axis++) {
+			for (double orbit = 0.5; orbit < s.length() && axis - orbit >= 0 && axis + orbit < s.length(); orbit++) {
+				if (s.charAt((int) (axis - orbit)) != s.charAt((int) (axis + orbit))) {
+					break;
+				}
+				count++;
+
+			}
+
+		}
+		return count;
+
+	}
+}
