e07a4bcffa9b578fab8f3cb4d84e0a9847260fc8
Prakhar Sharma <102613412+Prakhar788@users.noreply.github.com>
2022-11-18 09:29:09 +0530

    Time: 0 ms (100.00%), Space: 43.2 MB (23.01%) - LeetHub

--- 0206-reverse-linked-list/0206-reverse-linked-list.java
+++ 0206-reverse-linked-list/0206-reverse-linked-list.java
@@ -8,24 +8,41 @@
  *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
  * }
  */
+// class Solution {
+//     public ListNode reverseList(ListNode head) {
+//         if(head==null)
+//             return head;
+//         ListNode prev=null;
+//         ListNode temp=head;
+//         ListNode next=temp.next;
+//         while(temp!=null)
+//         {
+//             temp.next=prev;
+//             prev=temp;
+//             temp=next;
+//             if(next!=null)
+//             {
+//                 next=next.next;
+//             }
+//         }
+//         head=prev;
+//         return head;
+//     }
+// }
 class Solution {
     public ListNode reverseList(ListNode head) {
-        if(head==null)
+      if(head==null){
+          return null;
+      }
+        else if(head.next==null){
             return head;
-        ListNode prev=null;
-        ListNode temp=head;
-        ListNode next=temp.next;
-        while(temp!=null)
-        {
-            temp.next=prev;
-            prev=temp;
-            temp=next;
-            if(next!=null)
-            {
-                next=next.next;
-            }
         }
-        head=prev;
-        return head;
+        else{
+           ListNode nextNode=head.next;
+            head.next=null;
+            ListNode rest=reverseList(nextNode);
+            nextNode.next=head;
+            return rest;
+        }
     }
 }
