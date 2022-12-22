21106f249e9eacba87f7782c6a9a7182cd4bde01
Prakhar Sharma <102613412+Prakhar788@users.noreply.github.com>
2022-12-22 12:57:25 +0530

    Update Stock_Span.java

--- Stock_Span/Stock_Span.java
+++ Stock_Span/Stock_Span.java
@@ -1,23 +1,9 @@
-package Lec40;
-
-import java.util.Stack;
-
-public class Stock_Span {
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		int[] arr = { 91, 11, 13, 15, 38, 42, 37 };
-		span(arr);
-
-	}
-
-	public static void span(int[] arr) {
+public static void span(int[] arr) {
 		int[] span = new int[arr.length];
 		Stack<Integer> st = new Stack<>();
 
 		for (int i = 0; i < arr.length; i++) {
-			// Span
-
+			
 			while (!st.isEmpty() && arr[i] > arr[st.peek()]) {
 				st.pop();
 			}
@@ -30,10 +16,7 @@ public class Stock_Span {
 			st.push(i);
 
 		}
-		for (int i = 0; i < span.length; i++) {
-			System.out.println(arr[i] + " " + span[i]);
-		}
-
+		
 	}
 
 }
