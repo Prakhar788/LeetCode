fc9aef4e3071f7dfd182c4d5703748b633257cf2
Prakhar Sharma <102613412+Prakhar788@users.noreply.github.com>
2023-01-17 00:17:06 +0530

    Time: 41 ms (51.18%), Space: 43.1 MB (80.32%) - LeetHub

--- 0875-koko-eating-bananas/0875-koko-eating-bananas.java
+++ 0875-koko-eating-bananas/0875-koko-eating-bananas.java
@@ -11,21 +11,21 @@ class Solution {
         int hi=max;
         int speed=Integer.MAX_VALUE;
         while(low<=hi){
-            int mid=low +(hi-low)/2;//to avoid overflow
-            if(isPossible(piles,mid,h)==true){
-                speed=mid;
-                hi=mid-1;
+            int sp=low +(hi-low)/2;//to avoid overflow
+            if(isPossible(piles,sp,h)==true){
+                speed=sp;
+                hi=sp-1;
             }
             else{
-                low=mid+1;
+                low=sp+1;
             }
         }
         return speed;
     }
-    public boolean isPossible(int piles[],int mid,int h){
+    public boolean isPossible(int piles[],int sp,int h){
         int time=0;
         for(int i=0;i<piles.length;i++){
-            time+=(int)Math.ceil(piles[i]*1.0/mid);
+            time+=(int)Math.ceil(piles[i]*1.0/sp);
         }
         return time<=h;
     }
