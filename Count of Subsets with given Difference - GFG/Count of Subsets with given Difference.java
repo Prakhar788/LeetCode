d22c249a74691d3e73cd2d2e82a13fc594dbde8f
Prakhar Sharma <102613412+Prakhar788@users.noreply.github.com>
2023-01-18 18:50:52 +0530

    Create Count of Subsets with given Difference.java

---
+++ Count of Subsets with given Difference - GFG/Count of Subsets with given Difference.java
@@ -0,0 +1,35 @@
+int subsetSum(int a[], int n, int sum)
+{
+    // Initializing the matrix
+    int dp[n + 1][sum + 1];
+  // Initializing the first value of matrix
+    dp[0][0] = 1;
+    for (int i = 1; i <= sum; i++)
+        dp[0][i] = 0;
+    for (int i = 1; i <= n; i++)
+        dp[i][0] = 1;
+ 
+    for (int i = 1; i <= n; i++)
+    {
+        for (int j = 1; j <= sum; j++)
+        {
+          // if the value is greater than the sum
+            if (a[i - 1] <= j)
+                dp[i][j] = dp[i - 1][j] + dp[i - 1][j - a[i - 1]]; 
+            else
+                  dp[i][j] = dp[i - 1][j];
+        }
+    }
+
+    return dp[n][sum];
+}
+
+int countWithGivenSum(int arr[ ], int n, int diff)
+{   
+     int sum=0;
+    for(int i=0;i<n;i++)
+         sum+=nums[i]
+	 
+	 int reqSum=(diff+sum)/2;
+	 return subsetSum(arr,n,reqSum);
+}
