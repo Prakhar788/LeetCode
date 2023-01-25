c8e329e66274e1667ee2debabcba5cf5d7a3a424
Prakhar Sharma <102613412+Prakhar788@users.noreply.github.com>
2023-01-25 23:49:45 +0530

    Added solution - LeetHub

---
+++ Matrix Chain Multiplication - GFG/matrix-chain-multiplication.java
@@ -0,0 +1,58 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+import java.io.*;
+import java.util.*;
+
+class GFG
+{
+    public static void main(String args[])throws IOException
+    {
+        BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(in.readLine());
+        while(t-- > 0){
+            int N = Integer.parseInt(in.readLine());
+            String input_line[] = in.readLine().trim().split("\\s+");
+            int arr[]= new int[N];
+            for(int i = 0; i < N; i++)
+                arr[i] = Integer.parseInt(input_line[i]);
+            
+            Solution ob = new Solution();
+            System.out.println(ob.matrixMultiplication(N, arr));
+        }
+    }
+}
+
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+class Solution{
+    static int dp[][]=new int[101][101];
+    static int matrixMultiplication(int N, int arr[])
+    {
+        // code here
+        
+        for(int[] i:dp){
+            Arrays.fill(i,-1); }
+        int a=solve(arr,1,N-1);
+        return a;
+    }
+    static int solve(int arr[],int i,int j){
+        int ans=Integer.MAX_VALUE;
+        if(i>=j){
+            return 0;
+        }
+        if(dp[i][j]!=-1){
+            return dp[i][j];
+        }
+        for(int k=i;k<=j-1;k++){
+            int temp=solve(arr,i,k)+solve(arr,k+1,j)+(arr[i-1]*arr[j]*arr[k]);
+            if(temp<ans){
+                ans=temp;
+            }
+        }
+        return dp[i][j]=ans;
+    }
+}
