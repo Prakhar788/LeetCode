b0c6d60004c8d5a311ebebf8db1913381a498566
Prakhar Sharma <102613412+Prakhar788@users.noreply.github.com>
2023-04-17 09:50:31 +0530

    Time: 8 ms (94.41%), Space: 47.7 MB (28.12%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.java
+++ 0056-merge-intervals/0056-merge-intervals.java
@@ -1,19 +1,25 @@
 class Solution {
     public int[][] merge(int[][] intervals) {
-        Arrays.sort(intervals, (a, b) -> Integer.compare(a[0], b[0]));
-        LinkedList<int[]> merged = new LinkedList<>();
-        for (int[] interval : intervals) {
-            // if the list of merged intervals is empty or if the current
-            // interval does not overlap with the previous, simply append it.
-            if (merged.isEmpty() || merged.getLast()[1] < interval[0]) {
-                merged.add(interval);
+        List<int[]> answer = new ArrayList<>();
+        
+        if(intervals.length != 0 || intervals != null){
+            Arrays.sort(intervals, (a,b) -> a[0] - b[0]);
+            
+            int start = intervals[0][0];
+            int end = intervals[0][1];
+            for(int[] i: intervals){
+                if(i[0] <= end){
+                    end = Math.max(end, i[1]);
+                } else {
+                    answer.add(new int[]{start,end});
+                    start = i[0];
+                    end = i[1];
+                }
             }
-            // otherwise, there is overlap, so we merge the current and previous
-            // intervals.
-            else {
-                merged.getLast()[1] = Math.max(merged.getLast()[1], interval[1]);
-            }
+            answer.add(new int[]{start,end}); 
+            
         }
-        return merged.toArray(new int[merged.size()][]);
+        
+        return answer.toArray(new int[0][]); 
     }
 }
