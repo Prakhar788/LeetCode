fe2eebbb7d1c4e7067ead89209cebd30aca9d000
Prakhar Sharma <102613412+Prakhar788@users.noreply.github.com>
2023-06-21 23:28:06 +0530

    Time: 4 ms (77.64%), Space: 63.6 MB (72.78%) - LeetHub

---
+++ 2095-delete-the-middle-node-of-a-linked-list/2095-delete-the-middle-node-of-a-linked-list.java
@@ -0,0 +1,30 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode deleteMiddle(ListNode head) {
+        // Edge case: return nullptr if there is only one node.
+        if (head.next == null)
+            return null;
+        
+        // Initialize two pointers, 'slow' and 'fast'.
+        ListNode slow = head, fast = head.next.next;
+        
+        // Let 'fast' move forward by 2 nodes, 'slow' move forward by 1 node each step.
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        
+        // When 'fast' reaches the end, remove the next node of 'slow' and return 'head'.
+        slow.next = slow.next.next;
+        return head;
+    }
+}
