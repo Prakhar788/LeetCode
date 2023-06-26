c025348d4c413ada16db27f7cfa7f95c078ca8d7
Prakhar Sharma <102613412+Prakhar788@users.noreply.github.com>
2023-06-27 01:29:43 +0530

    Time: 45 ms (60.18%), Space: 50.3 MB (78.00%) - LeetHub

--- 0015-3sum/0015-3sum.java
+++ 0015-3sum/0015-3sum.java
@@ -1,31 +1,32 @@
 class Solution {
     public List<List<Integer>> threeSum(int[] nums) {
-        
-        List<List<Integer>> list = new ArrayList<List<Integer>>();
-        if(nums==null || nums.length<3){
-            return list;
-        }
-        Arrays.sort(nums);for(int i=0 ; i<nums.length-2 ; i++){
-            if(i ==0 || (i>0 && nums[i] != nums[i-1]) ){
-                int ans =0- nums[i];
-                int low = i+1;
-                int high = nums.length-1;
-                while(low < high){
-                       if(nums[low] + nums[high] == ans){
-            list.add(Arrays.asList(nums[i], nums[low] ,nums[high]));
-               while( nums[low] == nums[low+1] && low < nums.length-2 ) low++;
-               while(nums[high] == nums[high-1] && high>1) high--;
-               low++ ;
-               high--;
-                 }else{
-                        if(nums[low]+nums[high] < ans){
-                            low++;
-                        }else  high--;
-                 }
+        Arrays.sort(nums);
+        int n=nums.length;
+        List<List<Integer>> ans=new ArrayList<>();
+        for(int i=0;i<n;i++)
+        {
+            if(i>0 && nums[i]==nums[i-1]) 
+                continue;
+            int j=i+1,k=n-1;
+            while(j<k)
+            {
+                int sum=nums[i]+nums[j]+nums[k];
+                if(sum==0)
+                {
+                    ans.add(Arrays.asList(nums[i], nums[j], nums[k]));
+                    j++;
+                    k--;
+                    while(j<k && nums[j]==nums[j-1])
+                        j++;
+                    while(j<k && nums[k]==nums[k+1])
+                        k--;
                 }
-                
+                else if(sum<0)
+                    j++;
+                else
+                    k--;
             }
         }
-        return list;
+        return ans;
     }
 }
