9b941b0d17aadb9f167007cf3d764b32fec3ffea
Prakhar Sharma <102613412+Prakhar788@users.noreply.github.com>
2023-06-28 01:27:14 +0530

    Time: 27 ms (47.27%), Space: 44.4 MB (27.10%) - LeetHub

--- 0018-4sum/0018-4sum.java
+++ 0018-4sum/0018-4sum.java
@@ -5,32 +5,36 @@ class Solution {
 
         Arrays.sort(nums);
 
-        for (int i = 0; i < n; i++) {
-            for (int j = i + 1; j < n; j++) {
-
-                long target2 = (long) target - (long) nums[i] - (long) nums[j];
-                int lo = j + 1, hi = n - 1;
-
-                while (lo < hi) {
-                    int twoSum = nums[lo] + nums[hi];
-
-                    if (twoSum < target2) lo++;
-                    else if (twoSum > target2) hi--;
-                    else {
-                        List<Integer> quad = Arrays.asList(nums[i], nums[j], nums[lo], nums[hi]);
+        for(int i=0;i<n;i++){
+            if(i>0 && nums[i]==nums[i-1])continue;
+            for(int j=(i+1);j<n;j++){
+                if(j!=i+1 && nums[j]==nums[j-1])continue;
+                int l=j+1;
+                int k=n-1;
+                while(l<k){
+                    long sum=nums[i];
+                    sum+=nums[j];
+                    sum+=nums[l];
+                    sum+=nums[k];
+                    if(sum==target){
+                        List<Integer> quad = Arrays.asList(nums[i], nums[j], nums[l], nums[k]);
                         ans.add(quad);
-
-                        while (lo < hi && nums[lo] == quad.get(2)) lo++;
-                        while (lo < hi && nums[hi] == quad.get(3)) hi--;
+                        l++;
+                        k--;
+                        while(l<k && nums[k]==nums[k+1])k--;
+                        while(l<k && nums[l]==nums[l-1])l++;
                     }
+                    else if(sum>target){
+                        k--;
+                    }
+                    else{
+                        l++;    
+                    }
+                    
+                    
                 }
-
-                while (j + 1 < n && nums[j] == nums[j + 1]) j++;
             }
-
-            while (i + 1 < n && nums[i] == nums[i + 1]) i++;
         }
-
         return ans;
     }
 }
